62809c9c9e273661cd6b89faafa4f7cb8b582a00
AsravanthiR <88823383+AsravanthiR@users.noreply.github.com>
2022-01-17 22:28:43 +0200

    final version

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Maze from "./components/Maze";
+import Footer from "./components/Footer";
+const App = () => {
+    return (
+        <div className="App">
+        <center>
+          <Maze />
+        </center>
+        <Footer />
+
+            
+        </div>
+    );
+};
+
+export default App;
